461112227322d8d33de5ca30d6c309e1f3693174
rdelvallej32 <rdelvallej@gmail.com>
2016-10-29 08:55:21 -0400

    create a contact module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,25 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
-import { ContactComponent } from './components/contact/contact.component';
 
 import { HighlightDirective } from './highlight.directive';
-import { HighlightDirective as ContactHighlightDirective } from './components/contact/highlight.directive';
-
-import { AwesomePipe } from './components/contact/awesome.pipe'
 
 import { UserService } from './user.service';
-import { ContactService } from './components/contact/contact.service';
+
+/** Contact Imports */
+import { ContactModule } from './components/contact/contact.module';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, TitleComponent, HighlightDirective,
-        ContactComponent, AwesomePipe, ContactHighlightDirective],
+    imports: [BrowserModule, ContactModule],
+    declarations: [AppComponent, TitleComponent, HighlightDirective],
     bootstrap: [AppComponent],
-    providers: [UserService, ContactService]
+    providers: [UserService]
 })
 
 export class AppModule { }
