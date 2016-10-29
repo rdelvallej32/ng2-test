360275c421976f6eb423e305ca153c486ac45a9f
rdelvallej32 <rdelvallej@gmail.com>
2016-10-29 06:23:58 -0400

    add custom highlight directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: '<h1>My second Angular 2 App</h1>'
+    selector: 'my-app',
+    template: '<h1 highlight>My second Angular 2 App</h1>'
 })
 
 export class AppComponent { }
